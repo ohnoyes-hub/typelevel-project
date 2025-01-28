1eb27e6e0d3802bb2a8abebcf0aa5539a496f303
ohnoyes-hub <tdtsijpkens@gmail.com>
2025-01-28 20:03:47 +0100

    Database Configuration.

--- src/main/scala/com/ohnoyes/jobsboard/modules/Core.scala
+++ src/main/scala/com/ohnoyes/jobsboard/modules/Core.scala
@@ -2,32 +2,16 @@ package com.ohnoyes.jobsboard.modules
 
 import cats.effect.*
 import cats.implicits.*
-import doobie.hikari.HikariTransactor
-import doobie.util.*
 import com.ohnoyes.jobsboard.core.*
+import doobie.util.transactor.Transactor
 
 final class Core[F[_]] private (val jobs: Jobs[F])
 
 
-// spins up the core module(services)
 // postgres -> jobs -> core -> httpApi -> app
 object Core {
-
-    def postgresResource[F[_]: Async]: Resource[F, HikariTransactor[F]] = for {
-        ec <- ExecutionContexts.fixedThreadPool(32)
-        xa <- HikariTransactor.newHikariTransactor[F](
-            "org.postgresql.Driver",
-            "jdbc:postgresql:board", // TODO: move to config
-            "docker",
-            "docker",
-            ec
-        )
-    } yield xa
-
-    def apply[F[_]: Async]: Resource[F, Core[F]] =
-        postgresResource[F]
-            .evalMap(postgres => LiveJobs[F](postgres))
-            .map(jobs => new Core[F](jobs))
-
-
+    def apply[F[_]: Async](xa: Transactor[F]): Resource[F, Core[F]] =
+        Resource
+            .eval(LiveJobs[F](xa))
+            .map(jobs => new Core(jobs))
 }
