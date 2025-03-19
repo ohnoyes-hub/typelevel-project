e541bc7dd550034a71af027d3852742b73c6ac81
ohnoyes-hub <tdtsijpkens@gmail.com>
2025-03-19 18:51:53 +0100

    style job page.

--- app/src/main/scala/com/ohnoyes/jobsboard/pages/JobPage.scala
+++ app/src/main/scala/com/ohnoyes/jobsboard/pages/JobPage.scala
@@ -15,6 +15,25 @@ import com.ohnoyes.jobsboard.common.*
 import com.ohnoyes.jobsboard.domain.job.*
 import com.ohnoyes.jobsboard.components.*
 
+import scala.scalajs.* 
+import scala.scalajs.js.* // js.native
+import scala.scalajs.js.annotation.* // JSName
+
+@js.native
+@JSGlobal()
+class Moment extends js.Object {
+    def fromNow(): String = js.native // invokes the format() function in JS
+    def format(): String = js.native
+    // surface out any JS function as a new Scala method in this class
+}
+
+@js.native
+@JSImport("moment", JSImport.Default) // run a JS import statement
+object MomentLib extends js.Object{
+    def unix(date: Long): Moment = js.native
+}
+
+
 final case class JobPage(
     id: String,
     maybeJob: Option[Job] = None,
@@ -31,9 +50,10 @@ final case class JobPage(
             (setSuccessStatus("Yes, Job loaded").copy(maybeJob = Some(job)), Cmd.None)
         case _ => (this, Cmd.None)
     }
-    override def view(): Html[App.Msg] = maybeJob match
-        case Some(job) => renderJobPage(job)
-        case None => renderNoJobPage()
+    override def view(): Html[App.Msg] = renderNoJobPage()
+        // maybeJob match
+        // case Some(job) => renderJobPage(job)
+        // case None => renderNoJobPage()
     
         
 
@@ -43,20 +63,53 @@ final case class JobPage(
 
     // UI
     private def renderJobPage(job: Job) =
-        div(`class` := "job-page")(
-            div(`class` := "job-hero")(
-                img(
-                    `class` := "job-logo",
-                    src := job.jobInfo.image.getOrElse(""),
-                    alt := job.jobInfo.company
+        div(`class` := "container-fluid the-rock")(
+            div(`class` := "row jvm-jobs-details-top-card")(
+                div(`class` := "col-md-12 p-0")(
+                    div(`class` := "jvm-jobs-details-card-profile-img")(
+                        img(
+                        `class` := "img-fluid",
+                        src     := job.jobInfo.image.getOrElse(""),
+                        alt     := job.jobInfo.title
+                        )
+                    ),
+                    div(`class` := "jvm-jobs-details-card-profile-title")(
+                        h1(s"${job.jobInfo.company} - ${job.jobInfo.title}"),
+                        div(`class` := "jvm-jobs-details-card-profile-job-details-company-and-location")(
+                            JobComponents.renderJobSummary(job)
+                        )
+                    ),
+                    div(`class` := "jvm-jobs-details-card-apply-now-btn")(
+                        a(href := job.jobInfo.externalUrl, target := "blank")(
+                        button(`type` := "button", `class` := "btn btn-warning")("Apply now")
+                        ),
+                        p( 
+                            MomentLib.unix(job.date / 1000).fromNow()
+                        )
+                    )
+                )
+            ),
+            div(`class` := "container-fluid")(
+                div(`class` := "container")(
+                    div(`class` := "markdown-body overview-section")(
+                        renderJobDescription(job)
+                    )
                 ),
-                h1(s"${job.jobInfo.company} - ${job.jobInfo.title}")
-            ),
-            div(`class` := "job-overview")(
-                JobComponents.renderJobSummary(job)
-            ),
-            renderJobDescription(job),
-            a(href := job.jobInfo.externalUrl, `class` := "job-apply-action", target := "blank")("Apply")
+                div(`class` := "container")(
+                    div(`class` := "rok-last")(
+                        div(`class` := "row")(
+                            div(`class` := "col-md-6 col-sm-6 col-6")(
+                                span(`class` := "rock-apply")("Apply for this job.")
+                            ),
+                            div(`class` := "col-md-6 col-sm-6 col-6")(
+                                a(href := job.jobInfo.externalUrl, target := "blank")(
+                                button(`type` := "button", `class` := "rock-apply-btn")("Apply now")
+                                )
+                            )
+                        )
+                    )
+                )
+            )
         )
     
     // private def renderJobDetails(job: Job) = {
@@ -101,13 +154,19 @@ final case class JobPage(
     }
         
 
-    private def renderNoJobPage() = status.kind match {
-        case Page.StatusKind.LOADING => div("Loading...")
-        case Page.StatusKind.ERROR => 
-            div("Oh no! Page either doesn't exist or something went wrong")
-        case Page.StatusKind.SUCCESS =>
-            div("Hmmm, you reached this page despite server being healthy but no job...")
-    }
+    private def renderNoJobPage() = 
+        div(`class` := "container-fluid the-rock")(
+            div(`class` := "row jvm-jobs-details-top-card")(
+                status.kind match {
+                    case Page.StatusKind.LOADING => h2("Loading...")
+                    case Page.StatusKind.ERROR => 
+                        h2("Oh no! Page either doesn't exist or something went wrong")
+                    case Page.StatusKind.SUCCESS =>
+                        h2("Hmmm, you reached this page despite server being healthy but no job...")
+                }
+            )
+        )
+
         
 
     // logic
